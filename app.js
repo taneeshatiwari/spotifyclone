1dbecd7759790c04fa8eed14591cd1a99bd0c46e
taneeshatiwari <taneeshatiwari@gmail.com>
2024-01-26 00:00:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ let progressBar = document.getElementById('bar');
 let playingGif=document.getElementById('playingGif');
 let songItems = document.querySelectorAll('.songItems');
 
+audioElement.preload = "auto";
+
 let songs = [
     {songName:"Husn - Anuv Jain",filePath: "song/Husn.mp3",coverPath : "cover/Husn.png"},
     {songName:"Alag Aasmaan - Anuv Jain",filePath: "song/Alag Aasmaan.mp3",coverPath : "cover/Alag Aasmaan.png"},
